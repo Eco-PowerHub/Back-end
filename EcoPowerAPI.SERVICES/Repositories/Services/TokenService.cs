147fe01dfc0e0eb846ab2143231e5374810303ef
Nda Youssef <140206711+NdaYoussef@users.noreply.github.com>
2024-12-06 21:50:20 +0200

    upload serviceFile

---
+++ EcoPowerAPI.SERVICES/Repositories/Services/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using EcoPower.Models;
+using EcoPower.Repositories.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EcoPower.Repositories.Services
+{
+    public class TokenService :ITokenRepository
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public  string CreateToken (ApplicationUser user ,IList<string> roles)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,user.UserName!),
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
+                new Claim(JwtRegisteredClaimNames.Email,user.Email!),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role.ToString()));
+            }
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]!));
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken
+                (
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                claims : claims,
+                expires : DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+            
+        }
+    }
+}
