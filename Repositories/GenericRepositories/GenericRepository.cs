8d10dd86ed46f3f8b84c7ee6a308111194b49f20
NdaYoussef <ndayou99@gmail.com>
2025-02-10 20:36:09 +0200

    Add Authentication

--- Repositories/GenericRepositories/GenericRepository.cs
+++ Repositories/GenericRepositories/GenericRepository.cs
@@ -1,6 +1,49 @@
-﻿namespace EcoPowerHub.Repositories.GenericRepositories
+﻿
+using EcoPowerHub.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoPowerHub.Repositories.GenericRepositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        private readonly EcoPowerDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(EcoPowerDbContext context, DbSet<T> dbSet)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+          var models = await _dbSet.AsNoTracking().ToListAsync();
+            return models;
+        }
+        public async Task<T> GetById(int id)
+        {
+            var model = await _dbSet.FindAsync(id);
+            return model;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+           await _dbSet.AddAsync(entity);
+            return entity; 
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity); 
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+        public async Task<T> DeleteAsync(int id)
+        {
+            var model = await _dbSet.FindAsync(id);
+            if (model != null)
+            {
+                _dbSet.Remove(model);
+                await _context.SaveChangesAsync();
+            }
+            return model;
+        }
     }
 }
