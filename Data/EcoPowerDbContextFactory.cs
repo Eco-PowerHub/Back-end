5d6516ec8e93a27984c3689524ff7678ecd8ec99
NdaYoussef <ndayou99@gmail.com>
2025-03-25 23:34:11 +0200

    Add Base Package

---
+++ Data/EcoPowerDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EcoPowerHub.Data
+{
+    public class EcoPowerDbContextFactory : IDesignTimeDbContextFactory<EcoPowerDbContext>
+    {
+        public EcoPowerDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EcoPowerDbContext>();
+            // Replace with your MySQL connection string
+            optionsBuilder.UseMySql(
+                "Server=ecopower.czyqc6cssjrd.me-south-1.rds.amazonaws.com;Port=3306;Database=ecopower;User=admin;Password=Eco%Power123;",
+                ServerVersion.AutoDetect("Server=ecopower.czyqc6cssjrd.me-south-1.rds.amazonaws.com;Port=3306;Database=ecopower;User=admin;Password=Eco%Power123;")
+            );
+
+            return new EcoPowerDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Migrations/20250325213117_EditBasePackage.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoPowerHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditBasePackage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PackageType",
+                table: "Packages",
+                type: "varchar(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "BatteryCapacity",
+                table: "Packages",
+                type: "decimal(65,30)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BatteryLifespan",
+                table: "Packages",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Data",
+                table: "Packages",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BatteryCapacity",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "BatteryLifespan",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "Packages");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PackageType",
+                table: "Packages",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(13)",
+                oldMaxLength: 13)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
